600aa2c4e1dc03a5951bae4c6b9dae0392acc653
jmtoman <jmtoman@gmail.com>
2014-10-27 11:26:08 -0500

    www2check.ps1
    Logs into webpage and posts login details, looks for a 200 response and
    sends an email on failure.

---
+++ www2check.ps1
@@ -0,0 +1,29 @@
+﻿#Script requires PowerShell 3.0
+#Post login details to website and look for a 200 status code
+#Post parameters based on fields from page
+
+#create an array for the post fields
+$fields = @{tUN='username';tPW='password'}
+$website = "https://website.com/path.extension"
+$SmtpServer = "1.1.1.1"
+$SmtpFrom = "email@domain.com"
+$SmtpTo = "email@domain.com" 
+#
+$getresponse = Invoke-WebRequest -Uri $website 
+If($getresponse.StatusCode -notcontains "200")
+{
+Send-MailMessage -To $SmtpTo -Subject "$website GET Issue" -SmtpServer $SmtpServer -From NoReply@rsna.org -Body "GET to $website failed to respond with a 200 status code."
+}
+#create a variable out of the response of our post 
+$postresponse = Invoke-WebRequest -Uri https://www2.rsna.org/timssnet/login/caslogin.cfm -Method POST -Body $fields
+#create a variable if the previous does not contain a 200 status
+$failure = $postresponse | where {$_.StatusCode -notcontains "200"}
+#now we send an email if the failure variable exists
+If($failure)
+{
+Send-MailMessage -To $SmtpTo -Subject "$website POST Issue" -SmtpServer $SmtpServer -From NoReply@rsna.org -Body "Post to $website failed to respond with a 200 status code."
+}
+Else
+{
+Exit
+}
