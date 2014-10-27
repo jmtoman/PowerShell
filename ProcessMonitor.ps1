43de03df3d22f3ab8264b76eb02b2cfe4c4821af
jmtoman <jmtoman@gmail.com>
2014-10-27 11:32:30 -0500

    processmonitor.ps1
    A simple process monitor that restarts on failure and sends an email
    notice.

---
+++ ProcessMonitor.ps1
@@ -0,0 +1,18 @@
+﻿#Set the $program variable for the name of the process
+#Credit to DC for script
+#Computer/email alerting added 
+
+$Computer = Get-Content env:computername
+$Program = get-process '<process name>' -erroraction SilentlyContinue
+
+If ($Program -eq $null)
+{
+    Start-Process -filepath '<process location>'
+    Send-MailMessage -To email@domain.com -Subject "Scanner Process on $Computer Restarted" -SmtpServer 1.1.1.1 -From email@domain.com -Body "Scanner Process on $Computer Restarted"
+
+}
+Else
+{
+    exit
+
+} 
