38161327cd0bf161b91bbf94aec3e45c27f02b8f
jmtoman <jmtoman@gmail.com>
2018-01-11 14:49:46 -0600

    Add files via upload

---
+++ PureStorage-CopyVolumes.ps1
@@ -0,0 +1,15 @@
+﻿#simple script to copy volumes from a list, create a new volume with the date appended
+$api_token = "xxxxxxxxxxxxxxxx" 
+$array = 'xxxxxxxxxxxxxxx' 
+
+$FlashArray = New-PfaArray -EndPoint $array -ApiToken $api_token -IgnoreCertificateError 
+$datetime = get-date -format yyyymmddss
+
+#import list from text file
+$volumes = Get-Content C:\Files\Volumes.txt
+
+ForEach ($volume in $volumes)
+{
+    new-pfavolume -array $flasharray -source $volume -VolumeName $volume-$datetime
+}
+
