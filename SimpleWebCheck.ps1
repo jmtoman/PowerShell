f48a6268df91a4cb25021f8b8653d79d5eebb0bf
jmtoman <jmtoman@gmail.com>
2018-01-16 12:37:15 -0600

    Misc changes

--- SimpleWebCheck.ps1
+++ SimpleWebCheck.ps1
@@ -1,4 +1,4 @@
-﻿#Script requires PowerShell 3.0
+﻿
 #Post login details to website and look for a 200 status code
 #Post parameters based on fields from page
 
@@ -8,22 +8,18 @@ $website = "https://website.com/path.extension"
 $SmtpServer = "1.1.1.1"
 $SmtpFrom = "email@domain.com"
 $SmtpTo = "email@domain.com" 
-#
+
 $getresponse = Invoke-WebRequest -Uri $website 
 If($getresponse.StatusCode -notcontains "200")
 {
-Send-MailMessage -To $SmtpTo -Subject "$website GET Issue" -SmtpServer $SmtpServer -From NoReply@rsna.org -Body "GET to $website failed to respond with a 200 status code."
+    Send-MailMessage -To $SmtpTo -Subject "$website GET Issue" -SmtpServer $SmtpServer -From NoReply@rsna.org -Body "GET to $website failed to respond with a 200 status code."
 }
 #create a variable out of the response of our post 
-$postresponse = Invoke-WebRequest -Uri https://www2.rsna.org/timssnet/login/caslogin.cfm -Method POST -Body $fields
+$postresponse = Invoke-WebRequest -Uri $website -Method POST -Body $fields
 #create a variable if the previous does not contain a 200 status
 $failure = $postresponse | where {$_.StatusCode -notcontains "200"}
 #now we send an email if the failure variable exists
 If($failure)
 {
-Send-MailMessage -To $SmtpTo -Subject "$website POST Issue" -SmtpServer $SmtpServer -From NoReply@rsna.org -Body "Post to $website failed to respond with a 200 status code."
-}
-Else
-{
-Exit
+    Send-MailMessage -To $SmtpTo -Subject "$website POST Issue" -SmtpServer $SmtpServer -From $SmtpFrom -Body "Post to $website failed to respond with a 200 status code."
 }
