2b9118e110b4f59f56a3ec4808e0a8b6688bf4fd
jmtoman <jmtoman@gmail.com>
2018-07-24 10:18:55 -0500

    Add files via upload

---
+++ PureVolumesNoHostGroups.ps1
@@ -0,0 +1,16 @@
+﻿$api_token = "dcea5e9f-f535-de9e-24fa-cdff7f1451aa" 
+$ArrayAddress = '10.224.112.110' 
+
+$FlashArray = New-PfaArray -EndPoint $ArrayAddress -ApiToken $api_token -IgnoreCertificateError 
+$Volumes = Get-PfaVolumes -Array $FlashArray
+
+ForEach ($volume in $volumes)
+ 
+{
+  $HostGroups = Get-PfaVolumeHostGroupConnections -Array $FlashArray -VolumeName $volume.name
+  If ($HostGroups.host -eq $null)
+ 
+    {
+      Write-Host $volume.name
+    }
+}
